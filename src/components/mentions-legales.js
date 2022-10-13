7859b6adaae388cc3adc9ceae4374b79beeb114e
Pauline Gilg <gilgpauline@gmail.com>
2022-10-13 19:54:12 +0200

    Update legal notice

--- src/components/mentions-legales.js
+++ src/components/mentions-legales.js
@@ -6,8 +6,7 @@ function legalNotices() {
     <div>
       <h3 className="page-title">Mentions légales</h3>
       <p>Site web développé et géré par Pauline Gilg.</p>
-      <p>Hébergé chez <a href="https://www.ovh.com/fr/" target="_blank">OVH</a>, 2 Rue Kellermann, 59100 Roubaix.
-      </p>
+      <p>Hébergé par <a href="https://render.com/" target="_blank">Render</a>, 525 Brannan St Suite 300, San Francisco, CA 94107 États-Unis.</p>
     </div>
     
   );
